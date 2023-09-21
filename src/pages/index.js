68a20642ed1ce4dfccb24426e152a8cc72bbb66a
Matthew Bailin <matthewdbailin@gmail.com>
2023-09-21 16:28:05 +0700

    Changed SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default function IndexPage() {
     <ThemeProvider theme={theme}>
       <StickyProvider>
         <Layout>
-          <SEO title="Nam Project" />
+          <SEO title="Nam Housing Project" />
           <Banner />
           <Services />
           <WhyMe />
